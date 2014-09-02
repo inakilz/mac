9767858cdb92ddc25702471e86208c9e10833a27
root <root@lizarrios.(none)>
2014-09-02 19:05:36 +0200

    Update mac.php

--- mac.php
+++ mac.php
@@ -7,14 +7,14 @@
  */
 
 function getMacAddress() {
-	if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-		$output = shell_exec("ipconfig /all");
-		$match = preg_match("/(([0-9A-F]{2}[:-]){5}([0-9A-F]{2}))/i", $ifconfig, $output);
-		return $match ? $output[1] : NULL;
-	}
-	else {
-    	return shell_exec("/usr/sbin/arp -a | awk '{print $4}'");
-    }
+  if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+    $output = shell_exec("ipconfig /all");
+    $match = preg_match("/(([0-9A-F]{2}[:-]){5}([0-9A-F]{2}))/i", $ifconfig, $output);
+    return $match ? $output[1] : NULL;
+  }
+  else {
+    return shell_exec("/usr/sbin/arp -a | awk '{print $4}'");
+  }
 }
 
 ?>
