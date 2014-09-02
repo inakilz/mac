368927591916164cbd72de83a9a6a48c1dee762c
root <root@lizarrios.(none)>
2014-09-02 18:57:00 +0200

    added function to get the MAC address

---
+++ mac.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Function code to get the machine card physical (MAC) address.
+ * Supported Windows/LINUX system.
+ *
+ * @return string
+ */
+
+function getMacAddress() {
+	if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+		$output = shell_exec("ipconfig /all");
+		$match = preg_match("/(([0-9A-F]{2}[:-]){5}([0-9A-F]{2}))/i", $ifconfig, $output);
+		return $match ? $output[1] : NULL;
+	}
+	else {
+    	return shell_exec("/usr/sbin/arp -a | awk '{print $4}'");
+    }
+}
+
+?>
